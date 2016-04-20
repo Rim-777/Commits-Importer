080a85c064686617ab9653dfcbd87e9af8d7d6bd
Rim-777 <javatimomoss@gmail.com>
2016-04-20 19:28:22 +0300

    Add change to ReadMe

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe Import, type: :model do
 
 
   describe '#import!' do
-
+  let(:import){ Import.new('Rim-777', 'AskmeHow').import!}
     it 'should  receive get for RestClient class' do
       expect(RestClient).to receive(:get).with("https://api.github.com/repos/Rim-777/AskmeHow/commits?page=1")
-      Import.new('Rim-777', 'AskmeHow').import!
+      import
       end
 
     it 'should  receive parse for JSON class' do
       expect(JSON).to receive(:parse)
-      Import.new('Rim-777', 'AskmeHow').import!
+      import
     end
-    #
+
     it 'should  receive destroy_all for User class' do
       expect(User).to receive(:destroy_all)
-      Import.new('Rim-777', 'AskmeHow').import!
+      import
     end
-    #
+
     it 'should  save all commits in database' do
-      Import.new('Rim-777', 'AskmeHow').import!
+      import
       expect(Commit.all.size).to eq(220)
     end
   end
